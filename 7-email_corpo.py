0de8710c633473fa86fd37f27bb01512ee7813a8
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-07-26 13:37:33 -0300

    automação de email

---
+++ 7-email_corpo.py
@@ -0,0 +1,30 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+
+password = open('senha', 'r').read()
+from_email = 'pythonteste777@gmail.com'
+to_email = 'matheusaraujoguerra2603@outlook.com'
+subject = 'Proposta de parceria'
+body = open('files/corpo.txt', 'r', encoding='utf-8').read()
+
+# 1 - monstrando estrutura do email
+
+message = EmailMessage()
+message['from'] = from_email
+message['to'] = to_email
+message['Subject'] = subject
+
+message.set_content(body)
+safe = ssl.create_default_context()
+
+# 2 - Envio de email
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=safe) as smtp:
+    smtp.login(from_email, password)
+    smtp.sendmail(
+        from_email,
+        to_email,
+        message.as_string()
+    )
+    
