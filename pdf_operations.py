352bf2c506553821ff9c7890979d048a0c08d90c
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-05 00:01:48 -0300

    Convertendo Imagem em PDF(Pillow)

--- pdf_operations.py
+++ pdf_operations.py
@@ -1,6 +1,7 @@
 import PyPDF2 as pdf
 from PyPDF2 import PdfReader, PdfWriter
 import  os
+from PIL import Image
 
 def get_pdf_metadata(pdf_path):
     with open(pdf_path, 'rb') as f:
@@ -33,10 +34,27 @@ def split_pdf(pdf_path):
                 writer.write(out)
             print(f'PDF criado com o nome: {new_filename}')
 
+
+def convert_img_pdf(image_file):
+    my_image = Image.open(image_file)
+    # print(my_image)
+    img = my_image.convert('RGB')
+    # print(os.path.splitext(image_file)[0])
+    filename = f'{os.path.splitext(image_file)[0]}.pdf'
+    img.save(filename)
+
+
+
+
+
+
 # 1 - Buscando dados e Metadados de um arquivo pdf
 # print(get_pdf_metadata('files/sample.pdf'))
 # print(extract_text_from_pdf('files/sample.pdf'))
 
 # 2 - Dividindo PDF por página
-split_pdf('files/sample.pdf')
+# split_pdf('files/sample.pdf')
+
+# 7 - Converte imagem em pdf
+convert_img_pdf('files/img.png')
 
