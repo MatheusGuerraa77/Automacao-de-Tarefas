64eac8135e486878ddcd1577d3f1086c0014a206
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-04 22:18:14 -0300

    update

--- pdf_operations.py
+++ pdf_operations.py
@@ -1,5 +1,6 @@
 import PyPDF2 as pdf
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
+import  os
 
 def get_pdf_metadata(pdf_path):
     with open(pdf_path, 'rb') as f:
@@ -17,6 +18,24 @@ def extract_text_from_pdf(pdf_path):
             results.append(text)
         return ' '.join(results)
 
-#Buscando dados e Metadados de um arquivo pdf
-print(get_pdf_metadata('files/sample.pdf'))
-print(extract_text_from_pdf('files/sample.pdf'))
+
+def split_pdf(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        reader = PdfReader(f)
+        for page_num in range(0, len(reader.pages)):
+            selected_page = reader.pages[page_num]
+            writer = PdfWriter()
+            writer.add_page(selected_page)
+            # print(os.path.split(pdf_path)[1])
+            filename = os.path.split(pdf_path)[1]
+            new_filename = f'files/{filename}_{page_num+1}.pdf'
+            with open(new_filename, 'wb') as out:
+                writer.write(out)
+            print(f'PDF criado com o nome: {new_filename}')
+
+# 1 - Buscando dados e Metadados de um arquivo pdf
+# print(get_pdf_metadata('files/sample.pdf'))
+# print(extract_text_from_pdf('files/sample.pdf'))
+
+# 2 - Dividindo PDF por página
+split_pdf('files/sample.pdf')
